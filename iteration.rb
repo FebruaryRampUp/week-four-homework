71b60eed0796c9df820ddba9cc266c8a588c76ff
Justin Schoen <jaschoen@gmail.com>
2015-03-02 13:40:19 -0600

    pushing to justin branch

--- iteration.rb
+++ iteration.rb
@@ -2,19 +2,25 @@
 
 def average(number_array)
   # here we need to iterate over our array in order to find the average
+  total = 0
+  number_array.each do |number|
+  	total = total + number
+  end
+  total/number_array.count
 end
 
-average([10,20,30,40,50])
+puts average([10,20,30,40,50])
 # -> 30
 
 
 
-## Now, create a method that takes a hash with first and last name keys and outputs their full name as a string
+# Now, create a method that takes a hash with first and last name keys and outputs their full name as a string
 
-def full_name(name_hash)
-
+def full_name(hash_passed)
+	puts "The full name is #{hash_passed[:first_name]} #{hash_passed[:last_name]}"
 end
 
-penelope = {first_name: "Penelope", last_name: "Penn"}
-puts full_name(penelope)
+hash = {first_name: "Penelope", last_name: "Penn"}
+
+full_name(hash)
 # - > "Penelope Penn"
