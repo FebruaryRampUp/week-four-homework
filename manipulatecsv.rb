37194a13f0d7516b1d9e7ab2b6f3d3c0c985731d
zpalmer <zoepalmer2014@u.northwestern.edu>
2015-03-05 18:50:10 -0600

    cleaned up csv manipulation a little

--- manipulatecsv.rb
+++ manipulatecsv.rb
@@ -2,10 +2,6 @@ require 'csv'
 f = File.open('/Users/zpalmer/Documents/RampUp/week-four-homework/transaction.csv')
 data = CSV.parse(f.read)
 
-
-# [ [row0] [row1] [row2] ]
-#    col0   col1    col2
-
 # Find the necessary column, given a string name of a column and a parsed csv file
 def find_correct_column_number(column_name, data)
 	header_row = 0
@@ -17,13 +13,9 @@ def find_correct_column_number(column_name, data)
 	return "Column header not found."
 end
 
-
-
 # Find all of the customer_company_name values
 column_company_name = find_correct_column_number("customer_company_name", data)
-data.each do |row|
-	puts row[column_company_name]
-end
+data.each {|row| puts row[column_company_name]}
 
 # Find the sum of the transaction_charge values
 column_transaction_charge = find_correct_column_number("transaction_charge", data)
