7e20eede5587676293c00fed1d817e7d98811a8c
zpalmer <zoepalmer2014@u.northwestern.edu>
2015-03-05 19:12:46 -0600

    finished iteration exercises

--- iteration.rb
+++ iteration.rb
@@ -1,20 +1,25 @@
 ## Create a method called `average` that finds the average of a given array of numbers.
 
 def average(number_array)
-  # here we need to iterate over our array in order to find the average
+	sum = 0
+	number_array.each do |num|
+		sum += num
+	end
+	puts sum / number_array.length
 end
 
+puts "The average should be 30."
+puts "Average is "
 average([10,20,30,40,50])
-# -> 30
-
 
 
 ## Now, create a method that takes a hash with first and last name keys and outputs their full name as a string
 
 def full_name(name_hash)
-
+	return "#{name_hash[:first_name]} #{name_hash[:last_name]}"
 end
 
 penelope = {first_name: "Penelope", last_name: "Penn"}
+puts "This should display the string 'Penelope Penn'"
 puts full_name(penelope)
 # - > "Penelope Penn"
